75e8f95c13589e4f3d08e1d9ec477ba9c6ed9abc
unknown <ryan3465@gmail.com>
2026-02-16 10:28:27 +0000

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//import component A and component B
+import { A } from './a/a';
+import { B } from './b/b';
 
 @Component({
+  //root of the main component 
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //add the imports to the component
+  imports: [RouterOutlet, A, B],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-first-app');
+  //declare variables 
+  name:string = "Martin";
+  age:number = 21;
 }
